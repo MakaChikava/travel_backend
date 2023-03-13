062808bc952e361a7f56590af7296b5d4f785659
rominadouk <douk.romina@gmail.com>
2023-03-13 16:33:18 -0700

    edited server.js

--- server.js
+++ server.js
@@ -4,35 +4,46 @@ const mongoose = require('mongoose');
 const cors = require('cors')
 require('dotenv').config()
 const Weather = require('./models/weatherSchema.js')
+const packingData = require('./models/packingList.js')
+
+
 const rominaURI = process.env.MONGODB
 //Controllers
+
 //models
+const Item = require('./models/packingListSchema.js')
+
 //middleware
 app.use(express.json());
 app.use(cors());
 
 //ROUTES
-//replace createdlocation with whatever (just a placeholder for now)
 //CREATE
 app.post('/location', async (req,res) => {
-    const createdLocation = await Location.create(req.body)
+    const createdItem = await Item.create(req.body)
     res.json(createdLocation)
 });
 //INDEX
 app.get('/location', async(req,res) => {
-    const allLocations = await Location.find({})
-    res.json(allLocations)
+    const allItems = await Item.find({})
+    res.json(allItems)
 });
 
 //DELETE
 app.delete('/location/:id', async (req, res)=>{
-    const deleteLocation = await Location.findByIdandDelete(req.params.id);
-    res.json(deleteLocation);
+    const deletedItem = await Item.findByIdandDelete(req.params.id);
+    res.json(deletedItem);
 })
 //UPDATE
 app.put('/location/:id', async (req, res)=>{
-    const updateLocation = await Location.findByIdandUpdate(req.params.id, req.body, {new:true});
-    res.json(updateLocation);
+    const updatedItem = await Item.findByIdandUpdate(req.params.id, req.body, {new:true});
+    res.json(updatedItem);
+})
+
+//SEED
+app.post('/location/seed', async (req,res) => {
+    const seededItems = await Item.create(packingData);
+    res.json(seededItems)
 })
 
 mongoose.connect(rominaURI)
