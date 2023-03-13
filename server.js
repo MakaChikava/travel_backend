f36b7842a0850092d7df5727553ea4925f557360
makachikava <chikavamakanaka@gmail.com>
2023-03-13 14:27:33 -0700

    Delete and Update routes complete

--- server.js
+++ server.js
@@ -14,20 +14,26 @@ app.use(cors());
 //ROUTES
 //replace createdlocation with whatever (just a placeholder for now)
 //CREATE
-app.post('/', async (req,res) => {
+app.post('/location', async (req,res) => {
     const createdLocation = await Location.create(req.body)
     res.json(createdLocation)
 });
 //INDEX
-app.get('/', async(req,res) => {
+app.get('/location', async(req,res) => {
     const allLocations = await Location.find({})
     res.json(allLocations)
 });
 
 //DELETE
-
+app.delete('/location/:id', async (req, res)=>{
+    const deleteLocation = await Location.findByIdandDelete(req.params.id);
+    res.json(deleteLocation);
+})
 //UPDATE
-
+app.put('/location/:id', async (req, res)=>{
+    const updateLocation = await Location.findByIdandUpdate(req.params.id, req.body, {new:true});
+    res.json(updateLocation);
+})
 
 mongoose.connect(rominaURI)
 mongoose.connection.once('open', ()=>{
