ed4750f73ea21b40975bb38f520f20484d573b73
rominadouk <douk.romina@gmail.com>
2023-03-13 13:38:08 -0700

    added create route, index route, and middleware

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors')
 require('dotenv').config()
 
-MONGO_URI = ''
+const rominaURI = process.env.MONGODB
+//Controllers
+//models
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//ROUTES
+//replace createdlocation with whatever (just a placeholder for now)
+//CREATE
+app.post('/', async (req,res) => {
+    const createdLocation = await Location.create(req.body)
+    res.json(createdLocation)
+});
+//INDEX
+app.get('/', async(req,res) => {
+    const allLocations = await Location.find({})
+    res.json(allLocations)
+});
+
+//DELETE
+
+//UPDATE
 
 
-
-
-
-
-// mongoose.connect('')
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(rominaURI)
 mongoose.connection.once('open', ()=>{
     console.log('connected to mongoDB...');
 });
-
-
+                                            
 app.listen(3000, ()=>{
     console.log("listening")
-})
+});
