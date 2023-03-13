f143b6dc688f71d848a81022ac31cdf3b1f45ae2
makachikava <chikavamakanaka@gmail.com>
2023-03-13 13:40:06 -0700

    update serverjs

--- server.js
+++ server.js
@@ -4,31 +4,14 @@ const mongoose = require('mongoose');
 const cors = require('cors')
 require('dotenv').config()
 
-const rominaURI = process.env.MONGODB
-//Controllers
-//models
-//middleware
-app.use(express.json());
-app.use(cors());
-
-//ROUTES
-//replace createdlocation with whatever (just a placeholder for now)
-//CREATE
-app.post('/', async (req,res) => {
-    const createdLocation = await Location.create(req.body)
-    res.json(createdLocation)
-});
-//INDEX
-app.get('/', async(req,res) => {
-    const allLocations = await Location.find({})
-    res.json(allLocations)
-});
-
-//DELETE
-
-//UPDATE
+const rominaURI=process.env.MONGODB
 
 
+
+
+
+
+// mongoose.connect('')
 mongoose.connect(rominaURI)
 mongoose.connection.once('open', ()=>{
     console.log('connected to mongoDB...');
